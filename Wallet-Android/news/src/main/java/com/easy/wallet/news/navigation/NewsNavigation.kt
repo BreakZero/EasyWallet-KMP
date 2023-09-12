8b3efc1237e39d1eeecc888f7492028d3fd76738
BreakZero <dejin.po@gmail.com>
2023-09-12 10:50:46 +0800

    fix tab route

--- Wallet-Android/news/src/main/java/com/easy/wallet/news/navigation/NewsNavigation.kt
+++ Wallet-Android/news/src/main/java/com/easy/wallet/news/navigation/NewsNavigation.kt
@@ -7,12 +7,10 @@ import androidx.navigation.NavOptions
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import androidx.navigation.navigation
 import com.easy.wallet.news.NewsDetailScreen
 import com.easy.wallet.news.NewsRoute
 
 const val newsTabRoute = "_news_tab_route"
-internal const val newsRoute = "_news_route"
 internal const val newsDetailRoute = "_news_detail_route"
 
 fun NavController.selectedNewsTab(navOptions: NavOptions? = null) {
@@ -20,22 +18,19 @@ fun NavController.selectedNewsTab(navOptions: NavOptions? = null) {
 }
 
 fun NavGraphBuilder.newsGraph(navController: NavController) {
-    navigation(route = newsTabRoute, startDestination = newsRoute) {
-        composable(route = newsRoute) {
-            NewsRoute {
-                val encodedUrl = Uri.encode(it)
-                navController.navigate("${newsDetailRoute}/$encodedUrl")
-            }
-        }
-        composable(
-            route = "${newsDetailRoute}/{url}",
-            arguments = listOf(
-                navArgument("url") { type = NavType.StringType },
-            )
-        ) { backStackEntry ->
-            val url = backStackEntry.arguments?.getString("url").orEmpty()
-            NewsDetailScreen(url = Uri.decode(url), popBack = navController::popBackStack)
+    composable(route = newsTabRoute) {
+        NewsRoute {
+            val encodedUrl = Uri.encode(it)
+            navController.navigate("${newsDetailRoute}/$encodedUrl")
         }
     }
-
+    composable(
+        route = "${newsDetailRoute}/{url}",
+        arguments = listOf(
+            navArgument("url") { type = NavType.StringType },
+        )
+    ) { backStackEntry ->
+        val url = backStackEntry.arguments?.getString("url").orEmpty()
+        NewsDetailScreen(url = Uri.decode(url), popBack = navController::popBackStack)
+    }
 }
