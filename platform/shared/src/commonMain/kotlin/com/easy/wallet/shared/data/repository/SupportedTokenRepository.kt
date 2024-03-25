00eaef3524307b46077db5b819aa7e17db0e4168
BreakZero <dejin.po@gmail.com>
2024-03-25 18:03:23 +0800

    add mock data statement

--- platform/shared/src/commonMain/kotlin/com/easy/wallet/shared/data/repository/SupportedTokenRepository.kt
+++ platform/shared/src/commonMain/kotlin/com/easy/wallet/shared/data/repository/SupportedTokenRepository.kt
@@ -4,7 +4,6 @@ import com.easy.wallet.database.dao.ChainDao
 import com.easy.wallet.database.dao.LocalTokenDao
 import com.easy.wallet.model.TokenInformation
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlin.experimental.ExperimentalObjCRefinement
 import kotlin.native.HiddenFromObjC
@@ -14,16 +13,68 @@ class SupportedTokenRepository internal constructor(
     private val blockChainDao: ChainDao,
     private val tokenDao: LocalTokenDao
 ) {
+
+    /**
+     * Using for ios init default tokens
+     * look into database module migrations - 3.sqm
+     *
+     * Do not call before you sure you need add mock data
+     */
+    @HiddenFromObjC
+    suspend fun prepData() {
+        blockChainDao.addOne(
+            "Ethereum",
+            "https://ethereum.org/en/",
+            "https://etherscan.io",
+            "https://eth.llamarpc.com",
+            "1"
+        )
+        val chainId = blockChainDao.allChains().first().id
+        tokenDao.addOne(
+            "c60_t0xc00e94Cb662C3520282E6f5717214004A7f26888",
+            chainId,
+            "Compound",
+            "COMP",
+            18,
+            "0xc00e94Cb662C3520282E6f5717214004A7f26888",
+            "https://assets-cdn.trustwallet.com/blockchains/ethereum/assets/0xc00e94Cb662C3520282E6f5717214004A7f26888/logo.png",
+            "ERC20",
+            true
+        )
+        tokenDao.addOne(
+            "c60_t0x6B175474E89094C44Da98b954EedeAC495271d0F",
+            chainId,
+            "Dai",
+            "DAI",
+            18,
+            "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+            "https://assets-cdn.trustwallet.com/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
+            "ERC20",
+            true
+        )
+        tokenDao.addOne(
+            "ether_coin",
+            chainId,
+            "Ethereum",
+            "ETH",
+            18,
+            "",
+            "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png",
+            "",
+            true
+        )
+    }
+
     @HiddenFromObjC
     fun allSupportedTokenStream(): Flow<List<TokenInformation>> {
         return flow {
-            emit(tokenDao.allTokens())
+            emit(allSupportedToken())
         }
     }
 
     @HiddenFromObjC
     suspend fun allSupportedToken(): List<TokenInformation> {
-        return emptyList()
+        return tokenDao.allTokens()
     }
 
     @HiddenFromObjC
