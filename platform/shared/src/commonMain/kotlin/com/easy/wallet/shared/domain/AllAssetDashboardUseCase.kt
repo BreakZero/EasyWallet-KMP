45d059d2ca800c18f62683ae1fa5929fe81397de
BreakZero <dejin.po@gmail.com>
2024-05-07 11:50:49 +0800

    add group for multi coin

--- platform/shared/src/commonMain/kotlin/com/easy/wallet/shared/domain/AllAssetDashboardUseCase.kt
+++ platform/shared/src/commonMain/kotlin/com/easy/wallet/shared/domain/AllAssetDashboardUseCase.kt
@@ -4,8 +4,10 @@ import co.touchlab.kermit.Logger
 import com.easy.wallet.core.commom.AssetPlatformIdConstant
 import com.easy.wallet.model.Wallet
 import com.easy.wallet.model.asset.AssetBalance
+import com.easy.wallet.model.asset.AssetPlatform
 import com.easy.wallet.shared.data.repository.asset.CoinRepository
 import com.easy.wallet.shared.model.AllAssetDashboardInformation
+import com.ionspin.kotlin.bignum.decimal.BigDecimal
 import com.ionspin.kotlin.bignum.decimal.RoundingMode
 import com.ionspin.kotlin.bignum.decimal.toBigDecimal
 import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
@@ -27,39 +29,50 @@ class AllAssetDashboardUseCase internal constructor(
         return channelFlow {
             val coins = coinRepository.findAllCoin()
             val emptyBalances = coins.map { coin ->
-                val address = hdWallet.getAddressByPlatform(coin.platform.id)
+                val address = hdWallet.getAddressByPlatform(coin.platform)
                 AssetBalance.copyFromBasicCoin(coin, address, "0.00")
             }
+            val distinctCoins = emptyBalances.distinctBy { it.id }
             send(
                 AllAssetDashboardInformation(
                     fiatBalance = "888.88",
                     fiatSymbol = "USD",
-                    assetBalances = emptyBalances
+                    assetBalances = distinctCoins
                 )
             )
-            val assetBalances = emptyBalances.map { coin ->
+            val balanceMap = mutableMapOf<String, BigDecimal>()
+
+            // zip balance by coin id
+            emptyBalances.map { coin ->
                 coroutineScope {
                     val chainRepository = getChainRepositoryUseCase(coin.platform)
                     async {
                         val balance = try {
-                            chainRepository.loadBalance(platform = coin.platform, coin.address, coin.contract).toBigDecimal(
-                                exponentModifier = (-coin.decimalPlace).toLong()
-                            ).roundToDigitPositionAfterDecimalPoint(
-                                8,
-                                RoundingMode.ROUND_HALF_CEILING
-                            ).toPlainString()
-
+                            chainRepository.loadBalance(
+                                platform = coin.platform,
+                                coin.address,
+                                coin.contract
+                            ).toBigDecimal()
                         } catch (e: Exception) {
                             Logger.w("AllAssetDashboardUseCase: ") {
                                 e.message ?: "unknown error"
                             }
-                            "0.00"
+                            BigDecimal.ZERO
                         }
-                        coin.copy(balance = balance)
+                        if (balanceMap.containsKey(coin.id)) {
+                            balanceMap[coin.id] = balanceMap[coin.id]!!.plus(balance)
+                        } else {
+                            balanceMap[coin.id] = balance
+                        }
                     }
                 }
             }.awaitAll()
-
+            val assetBalances = distinctCoins.map {
+                val balance = (balanceMap[it.id] ?: BigDecimal.ZERO)
+                    .moveDecimalPoint(-it.decimalPlace)
+                    .roundToDigitPositionAfterDecimalPoint(8, RoundingMode.ROUND_HALF_CEILING)
+                it.copy(balance = balance.toPlainString())
+            }
             send(
                 AllAssetDashboardInformation(
                     fiatBalance = "888.88",
@@ -71,8 +84,8 @@ class AllAssetDashboardUseCase internal constructor(
     }
 }
 
-private fun HDWallet.getAddressByPlatform(platformId: String): String {
-    val coinType = when (platformId) {
+private fun HDWallet.getAddressByPlatform(platform: AssetPlatform): String {
+    val coinType = when (platform.id) {
         AssetPlatformIdConstant.PLATFORM_ETHEREUM, AssetPlatformIdConstant.PLATFORM_ETHEREUM_SEPOLIA -> CoinType.Ethereum
         else -> null
     }
